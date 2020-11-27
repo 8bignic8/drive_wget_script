9e3748eba366d6abe89d5251cfc4357bf3372aee
8bignic8 <nico42gieger@gmail.com>
2020-11-27 10:31:03 +0000

    copleation of the code notation and ReadMe file

--- README.md
+++ README.md
@@ -3,11 +3,14 @@ This is a smal script to help downloading data from google drive via wget
 
 #Start
 
-Start this script with
+Start this script in your terminal with
 
-
-python3 main.py
+>>>>>>> python3 main.py  <<<<<<<<<<<
 
 than answer the questions :)
 
 greetings
+
+thanks to @ https://medium.com/@acpanjan/download-google-drive-files-using-wget-3c2c025a8b99
+
+and Anjan Chandra Paudel
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
-link = input('Please insert link from drive.google.com: ')
-link_id = link.split('/')[5]
+link = input('Please insert link from drive.google.com: ') #reads the input of the user and saves it in link
+link_id = link.split('/')[5] #splits link at 5th backsalsh and saves it in link_id
 #print(link_id)
-dateinamen = input('please insert filename with file extension: ')
+dateinamen = input('please insert filename with file extension: ')#reads the input of the user and saves it in dateinamen
 #print(dateinamen)
 ausgabe = ("wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=")+link_id+(r"' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=")+link_id+("\" -O ")+dateinamen+(r" && rm -rf /tmp/cookies.txt")
-
-os.system(ausgabe)
+# puts togehter the string wir the generated variables above
+os.system(ausgabe) #puts out the generates string so that the system can perform it
